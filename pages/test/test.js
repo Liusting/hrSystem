0e5c82db524ed63b835a7263551ebe172969a063
Liusting <1326077352@qq.com>
2020-07-27 10:46:31 +0800

    提交

--- pages/test/test.js
+++ pages/test/test.js
@@ -5,6 +5,12 @@ var qqmapsdk = new QQMapWX({
 var App = getApp();
 Page({
   data: {
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
+ 
+    multiArray: [],
+    multiIndex: [0, 0, 0, 0],
+    chinaData: [],
     listInfo: [ //可删除，接口获取数据
       {
         "name": "佛山泛家居电商创意园",
@@ -38,6 +44,8 @@ Page({
         that.GetChangeJl(res.latitude, res.longitude)
       }
     })
+    
+    this.getSiteData();
 
   },
   onReady: function () {
@@ -85,4 +93,107 @@ Page({
       }
     })
   },
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindMultiPickerChange: function(e) {
+    console.log(e);
+  },
+  bindMultiPickerColumnChange: function(e) {
+    var move = e.detail;
+    var index = move.column;
+    var value = move.value;
+    if (index == 0) {
+      this.setData({
+        multiIndex: [value,0,0,0]
+      })
+      this.getCity();
+    }
+    if (index == 1) {
+      this.setData({
+        "multiIndex[1]": value,
+        "multiIndex[2]": 0,
+        "multiIndex[3]": 0
+      })
+      this.getXian();
+    }
+    if (index == 2) {
+      this.setData({
+        "multiIndex[2]": value,
+        "multiIndex[3]": 0,
+ 
+      })
+      this.getZhen();
+    }
+    if (index == 3) {
+      this.setData({
+        "multiIndex[3]": value
+      })
+      this.getZhen();
+    }
+  },
+  getSiteData: function() {
+
+    var that = this;
+    wx.request({
+      url: 'http://192.168.30.19:8082/spEmployee/getCityData',
+      success: res=> {
+        console.log(res);
+        var chinaData = res.data;
+        this.data.chinaData = chinaData;
+        var sheng = []; //  设置省数组
+        for(var i = 0; i < chinaData.length; i++) {
+          sheng.push(chinaData[i].name);
+        }
+        this.setData({
+          "multiArray[0]": sheng
+        })
+        that.getCity(); // 得到市
+      }
+    })
+  },
+  getCity: function() { //  得到市
+    var shengNum = this.data.multiIndex[0];
+    var chinaData = this.data.chinaData;
+    var cityData = chinaData[shengNum].children;
+    var city = [];
+    for (var i = 0; i < cityData.length; i++) {
+      city.push(cityData[i].name)
+    }
+    this.setData({
+      "multiArray[1]": city
+    })
+    this.getXian();
+  },
+  getXian: function(e) { //  得到县
+    var shengNum = this.data.multiIndex[0];
+    var cityNum = this.data.multiIndex[1];
+    var chinaData = this.data.chinaData;
+    var xianData = chinaData[shengNum].children[cityNum].children;
+    var xian = [];
+    for (var i = 0; i < xianData.length; i++) {
+      xian.push(xianData[i].name)
+    }
+    this.setData({
+      "multiArray[2]": xian
+    })
+    this.getZhen();
+  },
+  getZhen: function() { //  得到镇
+    var shengNum = this.data.multiIndex[0];
+    var cityNum = this.data.multiIndex[1];
+    var xianNum = this.data.multiIndex[2];
+    var chinaData = this.data.chinaData;
+    var zhenData = chinaData[shengNum].children[cityNum].children[xianNum].children;
+    var zhen = [];
+    for (var i = 0; i < zhenData.length; i++) {
+      zhen.push(zhenData[i].name)
+    }
+    this.setData({
+      "multiArray[3]" : zhen
+    })
+  }
 })
